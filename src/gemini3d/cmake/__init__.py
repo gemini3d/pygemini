12f0a9b90614d6848cd19e01ca8654e69cde2a3f
Michael Hirsch <scivision@users.noreply.github.com>
2020-07-21 11:08:33 -0400

    gemini-fortran version lock

---
+++ src/gemini3d/cmake/__init__.py
@@ -0,0 +1,1 @@
+# keep this file so Python can use this directory via importlib.resources
---
+++ src/gemini3d/cmake/git.cmake
@@ -0,0 +1,23 @@
+function(git_download root_dir url tag)
+
+find_package(Git REQUIRED)
+
+if(NOT IS_DIRECTORY ${root_dir})
+  execute_process(COMMAND ${GIT_EXECUTABLE} clone ${url} ${root_dir}
+    RESULT_VARIABLE _gitstat
+    TIMEOUT 120)
+  if(NOT _gitstat STREQUAL 0)
+    message(FATAL_ERROR "could not Git clone ${url}, return code ${_gitstat}")
+  endif()
+endif()
+
+# use WORKING_DIRECTORY for legacy HPC Git
+execute_process(COMMAND ${GIT_EXECUTABLE} checkout ${tag}
+  RESULT_VARIABLE _gitstat
+  TIMEOUT 30
+  WORKING_DIRECTORY ${root_dir})
+if(NOT _gitstat STREQUAL 0)
+  message(FATAL_ERROR "could not Git checkout ${tag}, return code ${_gitstat}")
+endif()
+
+endfunction(git_download)
