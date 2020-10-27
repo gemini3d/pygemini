2a565df2ae9848f50fdf4a9439d4c97f4cdff892
Michael Hirsch <scivision@users.noreply.github.com>
2020-10-27 00:36:07 -0400

    remove unused, conflicting import

--- src/gemini3d/__init__.py
+++ src/gemini3d/__init__.py
@@ -1,5 +1,4 @@
 from .readdata import read_config, readgrid, readdata, read_Efield
-from .compare import compare_all
 from .utils import get_cpu_count
 from .mpi import get_mpi_count
 from .base import get_simsize
