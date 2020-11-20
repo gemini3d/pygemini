afa5115d8b948cb0f5539dc8d28958fac959cf03
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-11-20 12:37:07 -0500

    git: make tag optional

--- src/gemini3d/cmake/git.cmake
+++ src/gemini3d/cmake/git.cmake
@@ -1,4 +1,5 @@
-function(git_download root_dir url tag)
+function(git_download root_dir url)
+# tag argument is optional
 
 find_package(Git REQUIRED)
 
@@ -11,13 +12,16 @@ if(NOT IS_DIRECTORY ${root_dir})
   endif()
 endif()
 
-# use WORKING_DIRECTORY for legacy HPC Git
-execute_process(COMMAND ${GIT_EXECUTABLE} checkout ${tag}
-  RESULT_VARIABLE _gitstat
-  TIMEOUT 30
-  WORKING_DIRECTORY ${root_dir})
-if(NOT _gitstat STREQUAL 0)
-  message(FATAL_ERROR "could not Git checkout ${tag}, return code ${_gitstat}")
+if(ARGC EQUAL 3)
+  set(tag ${ARGV2})
+  # use WORKING_DIRECTORY for legacy HPC Git
+  execute_process(COMMAND ${GIT_EXECUTABLE} checkout ${tag}
+    RESULT_VARIABLE _gitstat
+    TIMEOUT 30
+    WORKING_DIRECTORY ${root_dir})
+  if(NOT _gitstat STREQUAL 0)
+    message(FATAL_ERROR "could not Git checkout ${tag}, return code ${_gitstat}")
+  endif()
 endif()
 
 endfunction(git_download)
