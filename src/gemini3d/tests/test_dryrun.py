9a38ffd5098337f8c944c57743d4b47724231728
Michael Hirsch <scivision@users.noreply.github.com>
2020-12-07 14:41:27 -0500

    test_mpiexec: recognize that test should be that type is expected

--- src/gemini3d/tests/test_dryrun.py
+++ src/gemini3d/tests/test_dryrun.py
@@ -11,7 +11,12 @@ R = Path(__file__).parents[1] / "tests/data"
 def test_mpiexec():
     exe = job.get_gemini_exe()
     assert isinstance(exe, Path)
-    assert job.check_mpiexec("mpiexec", exe)
+
+    # It's OK if MPIexec doesn't exist, but make the test assert consistent with that
+    # there are numerous possibilities that MPIexec might not work
+    # predicting the outcome of this test requires the function we're testing!
+    mpiexec = job.check_mpiexec("mpiexec", exe)
+    assert isinstance(mpiexec, str) or mpiexec is None
 
 
 def test_dryrun(tmp_path):
