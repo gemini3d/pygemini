a9d5b502b3d38f068fb1de44d4c3283e86a1baf9
Michael Hirsch <scivision@users.noreply.github.com>
2021-01-13 15:08:32 -0500

    ci: test python 3.7, 3.8, 3.9

--- src/gemini3d/tests/test_plot.py
+++ src/gemini3d/tests/test_plot.py
@@ -2,6 +2,7 @@ import shutil
 from datetime import datetime
 import pytest
 from pathlib import Path
+import sys
 
 try:
     import gemini3d.plot
@@ -11,6 +12,7 @@ except ImportError as e:
 R = Path(__file__).parent / "data"
 
 
+@pytest.mark.skipif(sys.version_info < (3, 8), reason="Python >= 3.8 needed for copytree test")
 @pytest.mark.parametrize(
     "name",
     [
@@ -27,7 +29,7 @@ def test_plot(name, tmp_path):
     except ConnectionError as e:
         pytest.skip(f"failed to download reference data {e}")
 
-    shutil.copytree(test_dir, tmp_path)
+    shutil.copytree(test_dir, tmp_path, dirs_exist_ok=True)
     gemini3d.plot.frame(tmp_path, datetime(2013, 2, 20, 5), saveplot_fmt="png")
 
     plot_files = sorted((tmp_path / "plots").glob("*.png"))
