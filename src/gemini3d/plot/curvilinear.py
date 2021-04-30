def7ca06775e44420bb357bb0934d86871c3e669
Michael Hirsch <scivision@users.noreply.github.com>
2021-04-30 17:47:52 -0400

    update external library versions

--- src/gemini3d/plot/curvilinear.py
+++ src/gemini3d/plot/curvilinear.py
@@ -1,13 +1,14 @@
 from __future__ import annotations
 from datetime import datetime
 import numpy as np
+import xarray
 from matplotlib.figure import Figure
 
 
 def curv3d_long(
     time: datetime,
     grid: dict[str, np.ndarray],
-    parm: np.ndarray,
+    parm: xarray.DataArray,
     name: str,
     fg: Figure = None,
     **kwargs,
@@ -18,7 +19,7 @@ def curv3d_long(
 def curv2d(
     time: datetime,
     grid: dict[str, np.ndarray],
-    parm: np.ndarray,
+    parm: xarray.DataArray,
     name: str,
     fg: Figure = None,
     **kwargs,
