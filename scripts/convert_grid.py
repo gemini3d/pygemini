2c6fe6abf480af46e2709b6f3d436a1aa60a4f48
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2021-06-15 11:02:12 -0400

    convert_grid: use new parameters

--- scripts/convert_grid.py
+++ scripts/convert_grid.py
@@ -14,6 +14,7 @@ def cli():
     p = argparse.ArgumentParser()
     p.add_argument("format", help="file format", choices=["h5", "nc"])
     p.add_argument("indir", help="Gemini simgrid.dat")
+    p.add_argument("-i", "--intype", help="type of input file [.dat]", default=".dat")
     p.add_argument("-o", "--outdir", help="directory to write HDF5 files")
     P = p.parse_args()
 
@@ -30,14 +31,14 @@ def cli():
     outfile = outdir / (infile.stem + suffix)
     print(infile, "=>", outfile)
 
-    xg = read.grid(infile)
+    xg = read.grid(infile, file_format=P.intype)
 
     cfg = {
         "indat_size": infile.with_name(f"simsize{suffix}"),
         "indat_grid": infile.with_suffix(suffix),
     }
 
-    write.grid(cfg, xg)
+    write.grid(cfg, file_format=P.format, xg=xg)
 
 
 if __name__ == "__main__":
