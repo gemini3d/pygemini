54370225f70d7cedd40642bcf4e739750760d5bc
Matthew Zettergren <zettergm@Matthews-Mac-Pro.local>
2021-09-16 20:55:11 -0400

    fixed error in gridding of model data (detection of ndims)

--- src/gemini3d/grid/gridmodeldata.py
+++ src/gemini3d/grid/gridmodeldata.py
@@ -79,9 +79,16 @@ def model2magcoords(
     else:
         raise ValueError("Unsupported grid type...")
 
+    # count non singleton dimensions
+    numdims=0
+    for idim in range(0,3):
+        if xg["lx"][idim]>1:
+            numdims+=1
+
     # Execute plaid interpolation
     # [X1,X2,X3]=np.meshgrid(x1,x2,x3,indexing="ij")
-    if parm.ndim == 3:
+    #if parm.ndim == 3:     # problematic to not check for singleton dimensions
+    if numdims==3:
         # xi=np.zeros((x1i.size,3))
         xi = np.array((x1i.ravel(), x2i.ravel(), x3i.ravel())).transpose()
         parmi = scipy.interpolate.interpn(
@@ -92,7 +99,7 @@ def model2magcoords(
             bounds_error=False,
             fill_value=np.NaN,
         )
-    elif parm.ndim == 2:
+    elif numdims==2:
         coord1 = x1
         coord1i = x1i
         if parm.shape[1] == 1:
@@ -185,9 +192,15 @@ def model2geogcoords(
     else:
         raise ValueError("Unsupported grid type...")
 
+    # count non singleton dimensions
+    numdims=0
+    for idim in range(0,3):
+        if xg["lx"][idim]>1:
+            numdims+=1
+
     # Execute plaid interpolation
     # [X1,X2,X3]=np.meshgrid(x1,x2,x3,indexing="ij")
-    if parm.ndim == 3:
+    if numdims == 3:
         # xi=np.zeros((x1i.size,3))
         xi = np.array((x1i.ravel(), x2i.ravel(), x3i.ravel())).transpose()
         parmi = scipy.interpolate.interpn(
@@ -198,7 +211,7 @@ def model2geogcoords(
             bounds_error=False,
             fill_value=np.NaN,
         )
-    elif parm.ndim == 2:
+    elif numdims == 2:
         coord1 = x1
         coord1i = x1i
         if parm.shape[1] == 1:
