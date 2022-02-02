aaeb8440e8cf22a016f4ec83369b15b3018f848f
Michael Hirsch <scivision@users.noreply.github.com>
2022-02-02 15:54:29 -0500

    add msis_setup plain example

---
+++ Examples/msis_setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+"""
+example of using msis_setup
+"""
+
+from datetime import datetime
+import tempfile
+from pathlib import Path
+import numpy as np
+import logging
+
+import gemini3d.msis as gm
+
+
+logging.basicConfig(level=logging.INFO)
+
+
+tmp = tempfile.TemporaryDirectory()
+tmp_path = Path(tmp.name)
+
+print("msis_setup: files under directory", tmp_path)
+
+cfg = {
+    "time": [datetime(2015, 1, 2, 12)],
+    "f107": 100.0,
+    "f107a": 100.0,
+    "Ap": 4,
+    "msis_version": 0,
+    "msis_infile": tmp_path / "msis_in.h5",
+    "msis_outfile": tmp_path / "msis_out.h5",
+}
+
+(tmp_path / "inputs").mkdir(exist_ok=True)
+
+lx = (4, 2, 3)
+
+glon, alt, glat = np.meshgrid(
+    np.linspace(-147, -145, lx[1]),
+    np.linspace(100e3, 200e3, lx[0]),
+    np.linspace(65, 66, lx[2]),
+)
+xg = {
+    "glat": glat,
+    "glon": glon,
+    "lx": lx,
+    "alt": alt,
+}
+
+
+atmos = gm.msis_setup(cfg, xg)
+
+print(atmos)
