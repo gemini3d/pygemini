eee0bdb1e267abcbd13b7fe23e6b6fda5e573a63
Michael Hirsch <scivision@users.noreply.github.com>
2022-11-07 16:05:15 -0500

    convert-raw: update api

--- scripts/convert_data.py
+++ scripts/convert_data.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-convert Gemini data to HDF5 .h5
+convert Gemini3D old raw binary data to HDF5 .h5
 
 For clarity, the user must provide a config.nml for the original raw data.
 """
@@ -16,55 +16,48 @@ LSP = 7
 CLVL = 6
 
 
-def cli():
-    p = argparse.ArgumentParser()
-    p.add_argument("format", help="file format", choices=["h5", "nc"])
-    p.add_argument("indir", help="Gemini .dat file directory")
-    p.add_argument("-i", "--intype", help="type of input file [.dat]", default=".dat")
-    p.add_argument("-o", "--outdir", help="directory to write HDF5 files")
-    P = p.parse_args()
+p = argparse.ArgumentParser()
+p.add_argument("indir", help="Gemini .dat file directory")
+p.add_argument("-o", "--outdir", help="directory to write HDF5 files")
+P = p.parse_args()
 
-    indir = Path(P.indir).expanduser()
-    if P.outdir:
-        outdir = Path(P.outdir).expanduser()
-    elif indir.is_file():
-        outdir = indir.parent
-    elif indir.is_dir():
-        outdir = indir
-    else:
-        raise FileNotFoundError(indir)
+indir = Path(P.indir).expanduser()
+if P.outdir:
+    outdir = Path(P.outdir).expanduser()
+elif indir.is_file():
+    outdir = indir.parent
+elif indir.is_dir():
+    outdir = indir
+else:
+    raise FileNotFoundError(indir)
 
-    if indir.is_file():
-        infiles = [indir]
-        indir = indir.parent
-    elif indir.is_dir():
-        infiles = sorted(indir.glob(f"*{P.intype}"))
-    else:
-        raise FileNotFoundError(indir)
+if indir.is_file():
+    infiles = [indir]
+    indir = indir.parent
+elif indir.is_dir():
+    infiles = sorted(indir.glob("*.dat"))
+else:
+    raise FileNotFoundError(indir)
 
-    if not infiles:
-        raise FileNotFoundError(f"no {P.intype} files to convert in {indir}")
+if not infiles:
+    raise FileNotFoundError(f"no *.dat files to convert in {indir}")
 
-    cfg = read.config(indir)
-    if "flagoutput" not in cfg:
-        raise LookupError(f"need to specify flagoutput in {indir}/config.nml")
+cfg = read.config(indir)
+if "flagoutput" not in cfg:
+    raise LookupError(f"need to specify flagoutput in {indir}/config.nml")
 
-    try:
-        xg = raw_read.grid(indir, file_format=P.intype)
-    except FileNotFoundError:
-        xg = None
+try:
+    xg = raw_read.grid(indir)
+except FileNotFoundError:
+    xg = None
 
-    for infile in infiles:
-        if infile.name in {"simsize", "simgrid", "initial_conditions"}:
-            continue
+for infile in infiles:
+    if infile.name in {"simsize", "simgrid", "initial_conditions"}:
+        continue
 
-        outfile = outdir / (f"{infile.stem}.{P.format}")
-        print(infile, "=>", outfile)
+    outfile = outdir / (f"{infile.stem}.h5")
+    print(infile, "=>", outfile)
 
-        dat = raw_read.data(infile, file_format=P.intype, cfg=cfg, xg=xg)
+    dat = raw_read.data(infile, cfg=cfg, xg=xg)
 
-        write.data(outfile, dat, file_format=P.format, xg=xg)
-
-
-if __name__ == "__main__":
-    cli()
+    write.state(outfile, dat, xg=xg)
