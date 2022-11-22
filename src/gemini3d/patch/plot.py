48bec5ac913f0ae154b9c839ae165dbd1835388c
Michael Hirsch <scivision@users.noreply.github.com>
2022-11-22 12:47:01 -0500

    plot_patch: user fixed color limits

--- src/gemini3d/patch/plot.py
+++ src/gemini3d/patch/plot.py
@@ -21,26 +21,38 @@ def grid_step(x2new, x3new, i: int, N: int, ax: Axes) -> None:
 
 def patch(indir: Path, var: str):
 
+    # a priori parameter based on Gemini3D data structure in the HDF5 files and memory
     LSP = 7
     p4 = (0, 3, 2, 1)
     # p3 = (2, 1, 0)
 
-    ix1 = 10  # TODO arbitrary, needs to be physically references like in plot12, plot23, etc.
+    # arbitrary user parameters
+    clim_ne = (1e8, 1e11)
+
+    ix1 = 10  # TODO arbitrary, needs to be physically referenced like in plot12, plot23, etc.
 
     times = filenames2times(indir)
 
     for t in times:
+        first = True
+
         fg = figure()
         ax = fg.gca()
         ax.set_title(str(t))
+
         pat = utils.datetime2stem(t) + "_*.h5"
         for file in indir.glob(pat):
             with h5py.File(file, "r") as fh:
                 if var == "ne":
                     v = fh["/nsall"][:].transpose(p4)[LSP - 1, :, :, :]
+                    clim = clim_ne
 
             x2, x3 = patch_grid(file)
 
-            ax.pcolormesh(x2, x3, v[ix1, :, :].transpose())
+            h = ax.pcolormesh(x2, x3, v[ix1, :, :].transpose(), vmin=clim[0], vmax=clim[1])
+            if first:
+                fg.colorbar(h, ax=ax)
+                first = False
+
             draw()
             pause(0.05)
