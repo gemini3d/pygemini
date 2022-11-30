134f86e556b7e3eb0a45658bfb85c03928e3ce17
Michael Hirsch <scivision@users.noreply.github.com>
2022-11-30 11:16:03 -0500

    plot_patch: allow one or more variables

--- scripts/plot_patch.py
+++ scripts/plot_patch.py
@@ -9,18 +9,30 @@ import logging
 
 import gemini3d.patch.plot as plot
 
+plot_vars = {
+    "ne",
+    "Te",
+    "Ti",
+    "J1",
+    "J2",
+    "J3",
+    "v1",
+    "v2",
+    "v3",
+}
+
 
 p = argparse.ArgumentParser()
 p.add_argument("indir", help="ForestGemini patch .h5 data directory")
-p.add_argument("var", help="variable to plot")
+p.add_argument("-var", help="variable to plot", nargs="+", default=plot_vars)
 p.add_argument("-v", "--verbose", action="store_true")
-P = p.parse_args()
+args = p.parse_args()
 
-if P.verbose:
+if args.verbose:
     logging.basicConfig(level=logging.DEBUG)
 
-indir = Path(P.indir).expanduser()
+indir = Path(args.indir).expanduser()
 if not indir.is_dir():
     raise NotADirectoryError(indir)
 
-plot.patch(indir, P.var)
+plot.patch(indir, var=set(args.var))
--- src/gemini3d/patch/plot.py
+++ src/gemini3d/patch/plot.py
@@ -1,3 +1,4 @@
+import typing
 from pathlib import Path
 
 from matplotlib.pyplot import figure, Axes, draw, pause
@@ -19,15 +20,48 @@ def grid_step(x2new, x3new, i: int, N: int, ax: Axes) -> None:
     pause(0.05)
 
 
-def patch(indir: Path, var: str):
+def read_var(file: Path, var: set[str]) -> dict[str, typing.Any]:
 
     # a priori parameter based on Gemini3D data structure in the HDF5 files and memory
     LSP = 7
     p4 = (0, 3, 2, 1)
-    # p3 = (2, 1, 0)
+    p3 = (2, 1, 0)
+
+    dat = {}
+
+    with h5py.File(file, "r") as fh:
+        if {"ne", "ns", "v1", "Ti"} & var:
+            dat["ns"] = fh["/nsall"][:].transpose(p4)
+        if {"v1", "vs1"} & var:
+            dat["vs1"] = fh["/vs1all"][:].transpose(p4)
+        if {"Te", "Ti", "Ts"} & var:
+            dat["Ts"] = fh["/Tsall"][:].transpose(p4)
+        for k in {"J1", "J2", "J3"} & var:
+            dat[k] = fh[f"/{k}all"][:].transpose(p3)
+        for k in {"v2", "v3"} & var:
+            dat[k] = fh[f"/{k}avgall"][:].transpose(p3)
+        if "Phi" in var:
+            dat["Phi"] = fh["/Phiall"][:].transpose()
+
+    if {"ne", "v1", "Ti"} & var:
+        dat["ne"] = dat["ns"][LSP - 1, :, :, :]
+    if "v1" in var:
+        dat["v1"] = (dat["ns"][:6, :, :, :] * dat["vs1"][:6, :, :, :]).sum(axis=0) / dat["ne"]
+    if "Ti" in var:
+        dat["Ti"] = (dat["ns"][:6, :, :, :] * dat["Ts"][:6, :, :, :]).sum(axis=0) / dat["ne"]
+    if "Te" in var:
+        dat["Te"] = dat["Ts"][LSP - 1, :, :, :]
+
+    return dat
+
+
+def patch(indir: Path, var: set[str]) -> None:
 
     # arbitrary user parameters
-    clim_ne = (1e8, 1e11)
+    clim = {
+        "ne": (1e8, 1e11),
+        "Te": (100, 750),
+    }
     plot_dir = indir / "plots"
 
     ix1 = 10  # TODO arbitrary, needs to be physically referenced like in plot12, plot23, etc.
@@ -37,38 +71,38 @@ def patch(indir: Path, var: str):
     fg = figure()
 
     for t in times:
-        first = True
+        for k in var:
+            first = True
 
-        fg.clf()
-        ax = fg.gca()
-        ax.set_title(f"{var}: {t}")
+            fg.clf()
+            ax = fg.gca()
+            ax.set_title(f"{k}: {t}")
 
-        pat = utils.datetime2stem(t) + "_*.h5"
-        for file in indir.glob(pat):
-            wid = file.stem.split("_")[-1]
-            with h5py.File(file, "r") as fh:
-                if var == "ne":
-                    v = fh["/nsall"][:].transpose(p4)[LSP - 1, :, :, :]
-                    clim = clim_ne
+            pat = utils.datetime2stem(t) + "_*.h5"
+            for file in indir.glob(pat):
 
-            x2, x3 = patch_grid(file)
+                dat = read_var(file, {k})
 
-            h = ax.pcolormesh(x2, x3, v[ix1, :, :].transpose(), vmin=clim[0], vmax=clim[1])
-            ax.text(
-                x2[x2.size // 2],
-                x3[x3.size // 2],
-                wid,
-                verticalalignment="center",
-                horizontalalignment="center",
-            )
-            if first:
-                fg.colorbar(h, ax=ax)
-                first = False
+                x2, x3 = patch_grid(file)
 
-            draw()
-            pause(0.05)
+                h = ax.pcolormesh(
+                    x2, x3, dat[k][ix1, :, :].transpose(), vmin=clim[k][0], vmax=clim[k][1]
+                )
+                ax.text(
+                    x2[x2.size // 2],
+                    x3[x3.size // 2],
+                    s=file.stem.split("_")[-1],
+                    verticalalignment="center",
+                    horizontalalignment="center",
+                )
+                if first:
+                    fg.colorbar(h, ax=ax)
+                    first = False
 
-        pause(0.5)
-        plot_fn = plot_dir / f"{var}_{t:%Y-%m-%d_%H%M%S}.png"
-        print("save plot", plot_fn)
-        fg.savefig(plot_fn)
+                draw()
+                pause(0.05)
+
+            pause(0.5)
+            plot_fn = plot_dir / f"{k}_{t:%Y-%m-%d_%H%M%S}.png"
+            print("save plot", plot_fn)
+            fg.savefig(plot_fn)
