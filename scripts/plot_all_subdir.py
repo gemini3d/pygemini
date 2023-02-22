f8edb04794cda4466452439dea4abc996c8f85ca
scivision <scivision@users.noreply.github.com>
2023-02-22 03:10:45 -0500

    try pool

--- scripts/plot_all_subdir.py
+++ scripts/plot_all_subdir.py
@@ -7,8 +7,39 @@ to visually check for something unexpected.
 
 from pathlib import Path
 import argparse
+import concurrent.futures
+import itertools
 
 import gemini3d.plot as plot
+
+
+def plot_dir(d: Path, resume: bool) -> None:
+    if resume and (d / "plots").is_dir():
+        print("SKIP already plotted ", d)
+        return
+    if not (d / "inputs").is_dir():
+        print(f"SKIP no {d}/inputs dir", d)
+        return
+    print(d)
+    plot.plot_all(d)
+
+
+def print_dir(d: Path, resume: bool) -> None:
+    print(d)
+
+
+def main(top: Path, resume: bool) -> None:
+    """
+    With ThreadPoolExecutor and ProcessPoolExecutor:
+    exception not thrown till iterator is iterated to the exception.
+    """
+
+    dirs = (d for d in top.iterdir() if d.is_dir())
+
+    with concurrent.futures.ProcessPoolExecutor(max_workers=2) as executor:
+        res = executor.map(plot_dir, dirs, itertools.repeat(resume))
+
+    print(res)
 
 
 p = argparse.ArgumentParser()
@@ -23,13 +54,4 @@ top_dir = Path(P.top_dir).expanduser()
 if not top_dir.is_dir():
     raise NotADirectoryError(top_dir)
 
-dirs = (d for d in top_dir.iterdir() if d.is_dir())
-for d in dirs:
-    if P.resume and (d / "plots").is_dir():
-        print("SKIP already plotted ", d)
-        continue
-    if not (d / "inputs").is_dir():
-        print(f"SKIP no {d}/inputs dir", d)
-        continue
-    print(d)
-    plot.plot_all(d)
+main(top_dir, P.resume)
