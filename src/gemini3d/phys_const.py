95e0cfb284a02a5ff6e98d8ddcb971057ab84e74
zettergm <zettergm@zettergms-MacBook-Pro.local>
2024-02-20 09:19:22 -0500

    add constants file

---
+++ src/gemini3d/phys_const.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 16 21:12:57 2024
+
+Physical constants used in many calculations
+
+@author: zettergm
+"""
+
+import numpy as np
+
+kB=1.3806503e-23
+elchrg=1.60217646e-19
+amu=1.66053886e-27
+ms=np.array([16.,30.,28.,32.,14.,1.,9.1e-31/amu])*amu
+gammas=np.array([5/3,7/5,7/5,7/5,5/3,5/3,5/3])
+qs=np.array([1.,1.,1.,1.,1.,1.,-1.])*elchrg
+
