5a94a7546d93773999ba105509439c8ae1b9dbd6
scivision <scivision@users.noreply.github.com>
2024-03-05 12:40:16 -0500

    black formatting

--- src/gemini3d/phys_const.py
+++ src/gemini3d/phys_const.py
@@ -10,10 +10,9 @@ Physical constants used in many calculations
 
 import numpy as np
 
-kB=1.3806503e-23
-elchrg=1.60217646e-19
-amu=1.66053886e-27
-ms=np.array([16.,30.,28.,32.,14.,1.,9.1e-31/amu])*amu
-gammas=np.array([5/3,7/5,7/5,7/5,5/3,5/3,5/3])
-qs=np.array([1.,1.,1.,1.,1.,1.,-1.])*elchrg
-
+kB = 1.3806503e-23
+elchrg = 1.60217646e-19
+amu = 1.66053886e-27
+ms = np.array([16.0, 30.0, 28.0, 32.0, 14.0, 1.0, 9.1e-31 / amu]) * amu
+gammas = np.array([5 / 3, 7 / 5, 7 / 5, 7 / 5, 5 / 3, 5 / 3, 5 / 3])
+qs = np.array([1.0, 1.0, 1.0, 1.0, 1.0, 1.0, -1.0]) * elchrg
