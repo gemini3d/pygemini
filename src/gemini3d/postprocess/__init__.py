d0718315eac6b1533bad3d218ce9bb361f471183
Leslie Lamarche <leslie.lamarche@sri.com>
2026-02-20 21:27:22 -0800

    Add Mark's initial conductivity calculating script.

---
+++ src/gemini3d/postprocess/__init__.py
@@ -0,0 +1,16 @@
+"""
+postprocessing module
+
+Things to add based on mat-gemini:
+(https://github.com/gemini3d/mat_gemini-scripts/tree/main/%2Bgemscr/%2Bpostprocess)
+- Efield
+- GDIgrowth
+- GDIgrowth_intertial
+- Poynting
+- collisions3D
+- conductivity3D
+- conductivity_reconstruct
+- current_decompose
+- plot2field
+- vfield
+"""
